26de20d055fdb72ae4a740c5227cccaa9aa6536b
Siwasith <118959965+Siwasith@users.noreply.github.com>
2023-08-19 01:57:02 +0700

    navbar 1

--- main-project/react-app/src/pages/Navbar.js
+++ main-project/react-app/src/pages/Navbar.js
@@ -1,28 +1,41 @@
-import './css/navbar.css';
+import "./css/navbar.css";
 
 function Navbar() {
   return (
     <div className="navbar">
-      <div className='profile'>
-        <button className='profilePicture'><img src='.....' alt='profilePic'></img>profile</button>
-          <div className='information'>
-            <p>Username</p>
-            <a href='....'>Edit profile</a>
-          </div>
-      </div>
-      <div className='side-bar'>
-        <div className='side-bar-content'>
-          <h3><a href='....'>Home</a></h3>
-          
-          <h3><a href='....'>Bookmark</a></h3>
+      <div className="profile">
+        <img className="profilePicture" src="" alt=""></img>
 
-          <h3><a href='....'>Privacy setting</a></h3>
-        </div>
-        <div className='side-post-btn'>
-          <button className='post-btn'>Post</button>
+        <div className="information">
+          <p>Username</p>
+          <div class="hover-text">Click to edit profile</div>
         </div>
       </div>
-      
+      <div className="side-bar">
+        <div className="side-bar-content">
+          <span class="container2">
+            <div class="text2" data-text="Home">
+              Home
+            </div>
+          </span>
+
+          <span class="container1">
+            <div class="text1" data-text="Bookmark">
+              Bookmark
+            </div>
+          </span>
+
+          <span class="container1">
+            <div class="text1" data-text="Privacy">
+              Privacy
+            </div>
+          </span>
+        </div>
+
+        <div className="side-post-btn">
+          <button className="post-btn">Post</button>
+        </div>
+      </div>
     </div>
   );
 }
