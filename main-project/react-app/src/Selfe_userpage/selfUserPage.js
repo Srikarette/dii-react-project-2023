cd00b244d29402130a63ffe1c1bdd10bbbbc2f74
Samitthichai <113626752+Samitthichai@users.noreply.github.com>
2023-08-18 21:11:44 +0700

    uerself front-end

--- main-project/react-app/src/Selfe_userpage/selfUserPage.js
+++ main-project/react-app/src/Selfe_userpage/selfUserPage.js
@@ -1,4 +1,4 @@
-
+import './css/selfUser.css';
 
 function selfUser(){
     return (
@@ -8,19 +8,49 @@ function selfUser(){
           <button>Subscribe</button>
         </div>
         <div className = "backgroudprofile">
-<div className = "profile">
+            <div className="backgroudpro_img">
+            
+            </div>
 
-</div>
 <div className = "content">
-<div className = "details">
+    <div className = "profile">
+        {/* <img src="/main-project/react-app/src/pages/images/TestProfilePic.png" alt=""> */}
+    </div>
 
-<p className = "username"></p>
-<p className = "userdescript"></p>
-<button className = "editprofile">editprofile</button>
+<div className="detail">
+    <p className = "username"> UserName </p>
+    <p className = "userdescript"> Hello World !!!</p></div>
+
+<button className = "editprofile">Edit Profile</button>
 </div>
 </div>
-        </div>
-        </div>
+    
+
+<div className='post-container'>
+
+<div className='example-user-post' id='1'>
+          <div className='example-post-box'>
+            <div className='user-information'>
+              <button className='profilePic-btn'>Profile</button>
+              <div className='username-display'>
+                <p><a href='.....'>example_username</a></p>
+              </div>
+            </div>
+            <div className='post-content'>
+                <p>Lorem Ipsum is simply dummy text of the printing 
+                  and typesetting industry. Lorem Ipsum has been the industry's 
+                  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled 
+                  it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, 
+                  remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum 
+                  passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                  passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+            </div>
+          </div>
+</div>
+        </div> 
+       </div>
     )
+        
 }
 export default selfUser;
